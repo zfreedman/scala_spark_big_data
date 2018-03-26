95ac5a96d76625ac9ff9f809e46eb1b9baed8557
zfreedman <zfreedman92@gmail.com>
2018-03-26 03:02:34 -0700

    feat: Learned how to deal with numbers in Scala
    Learned how to deal with numbers in Scala. This includes integer,
    doubles, mathematical operations, and order of operations. Also figured
    out commenting.

---
+++ scala_level_one/arithmetic_and_numbers.scala
@@ -0,0 +1,34 @@
+/*
+Multi
+line
+comment
+*/
+// Single line comment
+
+// integer
+100
+// doules (floating point)
+2.5
+
+// addition
+1 + 1
+// subtraction
+2 - 1
+// multiplication
+2 * 5
+// integer division
+1 / 2
+// true division
+1.0 / 2
+
+// exponents
+math.pow(4, 2)
+
+// recalling a result (not sure why res4 is the 100 instead of res0 yet)
+res4
+// addition with res's
+res4 + res5
+
+// order of operations
+1 + 2 * 3 + 4
+(1 + 2) * (3 + 4)
