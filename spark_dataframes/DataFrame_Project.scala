797556abcf8278bf7f7cc3fddc5ebc8ec5daf762
zfreedman <zfreedman92@gmail.com>
2018-03-27 02:31:54 -0700

    feat: Finished dataframe project
    Finished dataframe project. This capstone just reviewed all of the Spark
    syntax revolving dataframes up till this point.

--- spark_dataframes/DataFrame_Project.scala
+++ spark_dataframes/DataFrame_Project.scala
@@ -3,35 +3,58 @@
 // the commented tasks below!
 
 // Start a simple Spark Session
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession.builder().getOrCreate()
 
 // Load the Netflix Stock CSV File, have Spark infer the data types.
+val df = spark.read.option(
+  "header", "true"
+).option(
+  "inferSchema", "true"
+).csv("Netflix_2011_2016.csv")
 
 // What are the column names?
+df.columns
 
 // What does the Schema look like?
+df.printSchema()
 
 // Print out the first 5 columns.
+df.head(5)
 
 // Use describe() to learn about the DataFrame.
+df.describe().show()
 
 // Create a new dataframe with a column called HV Ratio that
 // is the ratio of the High Price versus volume of stock traded
 // for a day.
+val df2 = df.withColumn("HVR", df("High") / df("Volume"))
 
 // What day had the Peak High in Price?
+df.orderBy($"High".desc).show(1)
 
 // What is the mean of the Close column?
+df.select(mean("Close")).show()
 
 // What is the max and min of the Volume column?
+df.select(max("Volume")).show()
+df.select(min("Volume")).show()
 
 // For Scala/Spark $ Syntax
+import spark.implicits._
 
 // How many days was the Close lower than $ 600?
+df.filter("Close < 600").collect().length
+// df.filter("Close < 600").count() also good
 
 // What percentage of the time was the High greater than $500 ?
-
+df.filter("High > 500").collect().length * 1.0 / df.count
 // What is the Pearson correlation between High and Volume?
-
+df.select(corr("High", "Volume")).show()
 // What is the max High per year?
-
+val df3 = df.withColumn("Year", year(df("Date")))
+df3.select($"Year", $"High").groupBy("Year").max().show()
 // What is the average Close for each Calender Month?
+val df4 = df.withColumn(
+  "Month", month(df("Date"))).groupBy("Month").mean()
+df4.select($"Month", $"avg(Close)").orderBy($"Month").show()
